71827b5b41c892440aa5c426278a4356a3e258e1
paulinamarkiewicz641 <paulinamarkiewicz4506@gmail.com>
2025-06-09 13:56:01 +0200

    Report 2

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
-def create_report (data_file_name: str, report_file_name: str) -> None:
+def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_total = 0
     buy_total = 0
 
-
-    with open(data_file_name, 'r') as file:
+    with open(data_file_name, "r") as file:
         for line in file:
             line = line.strip()
             if not line:
                 continue
             operation: str
             amount_str: str
-            operation, amount_str = line.split(',')
+            operation, amount_str = line.split(",")
             amount = int(amount_str)
-            if operation == 'supply':
+            if operation == "supply":
                 supply_total += amount
-            elif operation == 'buy':
+            elif operation == "buy":
                 buy_total += amount
 
     result: int = supply_total - buy_total
@@ -24,6 +23,5 @@ def create_report (data_file_name: str, report_file_name: str) -> None:
         f'result,{result}'
     ]
 
-
-    with open(report_file_name, 'w') as file:
-        file.write('\n'.join(report_lines))
+    with open(report_file_name, "w") as file:
+        file.write("\n".join(report_lines))
